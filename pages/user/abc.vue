3b304f3f473848ea448869735a046885f33b16e4
grantpan <grantpan@yeahka.com>
2020-10-27 20:18:52 +0800

    初次提交

---
+++ pages/user/abc.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>user page abc</div>
+</template>
+
+<script>
+export default {
+    name: "userabcPage"
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>user page index
+
+       <a href="/">a标签 会刷新页面</a>
+       <router-link to="/">router-link 不刷新</router-link>  
+
+       <p @click="jumpIndex">编程式导航</p>
+  </div>
+ 
+</template>
+
+<script>
+export default {
+    name: "userPage",
+    methods: {
+        jumpIndex(){
+            this.$router.push('/')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
